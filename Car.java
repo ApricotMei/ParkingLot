8c2f205089e48b898080f480a2331c23a3d73920
Xinmei Jin <33533331+ApricotMei@users.noreply.github.com>
2019-11-16 18:22:41 -0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,12 @@
+public class Car extends Vehicle{
+
+    public Car(String plate, String type) { //type = sedan, cooper etc.
+        super(plate, type);
+    }
+
+    @Override
+    public String getType() {
+        return this.getClass().getSimpleName();
+    }
+}
+
---
+++ Truck.java
@@ -0,0 +1,11 @@
+public class Truck extends Vehicle{
+
+    public Truck(String plate, String type) {   //type = SUV etc.
+        super(plate, type);
+    }
+
+    @Override
+    public String getType() {
+        return this.getClass().getSimpleName();
+    }
+}
